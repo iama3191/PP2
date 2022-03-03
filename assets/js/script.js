9f71c1dccde1588bd6019b2dc9cd2544b31a0345
Irene <ire.andreina.z@gmail.com>
2022-03-03 15:19:22 +0100

    add function to run the game, and compare players' choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,22 +7,40 @@ document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
+
         button.addEventListener("click", function() {
             let optionType = this.getAttribute("data-type");
-            alert(optionType);
-            computerChoice();
+            console.log(`1 user's choice is ${optionType}`)
+            mainGame(optionType);
+            
         })
     }
 })
 
-//function for computer's choice
+/**
+ * function for getting a random option for the computer
+ */
+function computerRandomChoice() {
 
-function computerChoice() {
     let randomChoice = gameOptions[Math.floor(Math.random() * gameOptions.length)];
-    console.log(randomChoice);
     return randomChoice;
 }
 
+//function for run the game 
+function mainGame(userChoice) {
+
+    let computerChoice = computerRandomChoice();
+    console.log(`2 computer's choice is ${computerChoice}`)
+    if (userChoice === computerChoice) {
+        console.log("this is a draw!");
+    } else { //if userChoice is different to computerSelection
+        if (((userChoice === 'rock') && (computerChoice === 'scissors')) || ((userChoice === 'paper') && (computerChoice === 'rock')) || ((userChoice === 'scissors') && (computerChoice === 'paper'))) {
+            console.log(`You win! ${userChoice} beats ${computerChoice}`);
+        } else {
+            console.log(`You loose! ${computerChoice} beats ${userChoice}`);
+        }
+    }
+}
 //function for comparing user and computer's choice
 
 //function to update the score container
