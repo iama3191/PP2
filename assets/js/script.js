bdae395d4487f9af2ceb3e58bb9e70122056100a
Irene <ire.andreina.z@gmail.com>
2022-03-03 11:46:36 +0100

    add event listener to the user's options and a DOM listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,19 @@
 //Declaring constants
-
 const gameOptions = ['rock','paper','scissors'];
 
-//add eventListener to know what button is clicking the user
+//Wait for the DOM to finish loading before running the game
+//Get the button elements and add event listeners to them
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            let optionType = this.getAttribute("data-type");
+            alert(optionType);
+        })
+    }
+})
+//add eventListener to know what button is clicking the user  runGame(optionType);
 
 //function for computer's choice
 
