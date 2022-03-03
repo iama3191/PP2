1b11a020ad32269b9334f0f9f78d4cffdfb6ab93
Irene <ire.andreina.z@gmail.com>
2022-03-03 11:35:58 +0100

    add const for available choices (rock, paper, scissors) and comments  for needed functions

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+//Declaring constants
+
+const gameOptions = ['rock','paper','scissors'];
+
+//add eventListener to know what button is clicking the user
+
+//function for computer's choice
+
+//function for comparing user and computer's choice
+
+//function to update the score container
+
+//function to update the round counter
